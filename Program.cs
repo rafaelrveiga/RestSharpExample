ca02f80eaaca9e2b7fa8b9e1597186d0053b5c3b
Rafael Veiga <rafaelrveiga@gmail.com>
2022-02-04 17:57:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using RestSharp;
+
+var cep = 15370496; //CEP Avenida Paulista
+var client = new RestClient($"https://viacep.com.br/ws/{cep}/json/");
+var response = await client.ExecuteGetAsync(new RestRequest());
+Console.WriteLine(response.Content);
